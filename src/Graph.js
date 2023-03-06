711cd099e67a05c0bd50210b3bf38bf2d0d129f2
Ryo Suzuki <ryosuzk@gmail.com>
2023-03-06 09:23:20 -0700

    Draw a graph based on the math equation

--- src/Graph.js
+++ src/Graph.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Line } from 'react-konva'
+import { parse, compile } from 'mathjs'
+import { parseTex, evaluateTex } from 'tex-math-parser'
 
 class Graph extends Component {
   constructor(props) {
     super(props)
     window.Graph = this
     this.state = {
+      equation: 'x^2',
       points: [],
       origin: { x: 0, y: 0 },
       ratio: { x: 0, y: 0 },
@@ -21,7 +24,10 @@ class Graph extends Component {
   init() {
     let points = []
     for (let x = -3; x < 3; x += 0.01) {
-      let y = (x)**2
+      // let y = x**2
+      // let str = "x^2+3x-10 \\sin \\left( 2x \\right)"
+      let answer = evaluateTex(this.state.equation, { x: x });
+      let y = answer.evaluated
       points.push(x, y)
     }
     let origin = { x: 659, y: 947 }
@@ -31,11 +37,23 @@ class Graph extends Component {
     let ys = points.filter((el, i) => i % 2 === 1)
     let bounding = { minX: _.min(xs), maxX: _.max(xs), minY: _.min(ys), maxY: _.max(ys) }
     let xRatio = (graphBounding.maxX - graphBounding.minX) / (bounding.maxX - bounding.minX)
-    console.log(xRatio)
     let yRatio = - (graphBounding.maxY - graphBounding.minY) / (bounding.maxY - bounding.minY)
-    console.log(xRatio, yRatio)
     let ratio = { x: xRatio, y: yRatio }
     this.setState({ origin: origin, graphBounding: graphBounding, axisBounding: axisBounding, ratio: ratio, points: points })
+  }
+
+  update(equation) {
+    try {
+      let points = []
+      for (let x = -3; x < 3; x += 0.01) {
+        let answer = evaluateTex(equation, { x: x });
+        let y = answer.evaluated
+        points.push(x, y)
+      }
+      this.setState({ equation: equation, points: points })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   convert(points) {
@@ -57,10 +75,7 @@ class Graph extends Component {
         <Line
           x={ 0 }
           y={ 0 }
-          points={ this.state.points.map((el, i) => {
-            if (i % 2 === 0) return el * this.state.ratio.x + this.state.origin.x
-            if (i % 2 === 1) return el * this.state.ratio.y + this.state.origin.y
-          }) }
+          points={ this.convert(this.state.points) }
           stroke={ App.strokeColor }
           strokeWidth={ 4 }
           draggable
