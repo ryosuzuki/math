1d5575cb09cc43433300f7b80f9201db45cc0a8c
Ryo Suzuki <ryosuzk@gmail.com>
2023-03-06 14:51:28 -0700

    Disable camera contro

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ class App extends Component {
         <img id='paper' src={ `${this.domain}/public/sample/sample-${this.sampleId}.jpg` } crossOrigin='anonymous' style={{ display: 'none' }} />
         { isCameraOn ? '' :
           <a-scene>
-            <a-camera id="camera" position="0 1.5 -0.4" look-controls="enabled: true" raycaster="objects: .cantap" cursor="fuse: false; rayOrigin: mouse;"></a-camera>
+            <a-camera id="camera" position="0 1.5 -0.4" look-controls="enabled: false" raycaster="objects: .cantap" cursor="fuse: false; rayOrigin: mouse;"></a-camera>
             <a-plane drawing-plane id="drawing-plane" class="cantap" position="0 1.5 -1" width="1" height="1" color="#ccc" opacity="0">
             </a-plane>
           </a-scene>
