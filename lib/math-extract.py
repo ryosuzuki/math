a0305eed6c1eed86eb898b8e8bfe05fa094e9dec
Ryo Suzuki <ryosuzk@gmail.com>
2023-03-26 19:04:44 -0600

    Get target and mathpix 08-10

--- lib/math-extract.py
+++ lib/math-extract.py
@@ -1,14 +1,14 @@
 import json
 from cnstd import LayoutAnalyzer
 
-# sample_ids = ['1', '2', '3', '4', '5', '6', '7']
-num = 10
+# sample_ids = [1, 2, 3, 4]
+snum = 10
 sample_ids = list(range(1, num+1))
 path = '../public/sample'
 
 for sample_id in sample_ids:
-  img = path + '/sample-' + str(sample_id) + '.jpg'
-  out = path + '/math-' + str(sample_id) + '.json'
+  img = path + '/sample-{:02d}.jpg'.format(sample_id)
+  out = path + '/math-{:02d}.jpg'.format(sample_id)
   analyzer = LayoutAnalyzer('mfd')
   equations = analyzer.analyze(img)
 
